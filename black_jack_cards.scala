1f5349be7cde081465024082f4fa3495e72fafed
Bo Cheng <bocheng1992@gmail.com>
2020-08-30 23:59:28 -0400

    Add files via upload

---
+++ black_jack_cards.scala
@@ -0,0 +1,67 @@
+/**
+ *the following is assigning values for each of the cards for black jack game
+ */
+
+//Heart
+val twoHeart: Int = 2
+val threeHeart: Int = 3
+val fourHeart: Int = 4
+val fiveHeart: Int = 5
+val sixHeart: Int = 6
+val sevenHeart: Int = 7
+val eightHeart: Int = 8
+val nineHeart: Int = 9
+val tenHeart: Int = 10
+val jackHeart: Int = 10
+val queenHeart: Int = 10
+val kingHeart: Int = 10
+
+var aceHeart: Int = 1
+
+// Spades
+val twoSpades: Int = 2
+val threeSpades: Int = 3
+val fourSpades: Int = 4
+val fiveSpades: Int = 5
+val sixSpades: Int = 6
+val sevenSpades: Int = 7
+val eightSpades: Int = 8
+val nineSpades: Int = 9
+val tenSpades: Int = 10
+val jackSpades: Int = 10
+val queenSpades: Int = 10
+val kingSpades: Int = 10
+
+var aceSpades: Int = 1
+
+// Diamonds
+val twoDiamonds: Int = 2
+val threeDiamonds: Int = 3
+val fourDiamonds: Int = 4
+val fiveDiamonds: Int = 5
+val sixDiamonds: Int = 6
+val sevenDiamonds: Int = 7
+val eightDiamonds: Int = 8
+val nineDiamonds: Int = 9
+val tenDiamonds: Int = 10
+val jackDiamonds: Int = 10
+val queenDiamonds: Int = 10
+val kingDiamonds: Int = 10
+
+var aceDiamonds: Int = 1
+
+// Clubs
+val twoClubs: Int = 2
+val threeClubs: Int = 3
+val fourClubs: Int = 4
+val fiveClubs: Int = 5
+val sixClubs: Int = 6
+val sevenClubs: Int = 7
+val eightClubs: Int = 8
+val nineClubs: Int = 9
+val tenClubs: Int = 10
+val jackClubs: Int = 10
+val queenClubs: Int = 10
+val kingClubs: Int = 10
+
+var aceClubs: Int = 1
